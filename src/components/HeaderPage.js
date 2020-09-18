da8ae4c6b40e85e495009a3b76b2a6f898fa751e
Leif Tukums <leiftukums@gmail.com>
2020-09-18 18:32:15 -0400

    stored user data im user area

--- src/components/HeaderPage.js
+++ src/components/HeaderPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
 
 export const Header = ({ startLogout }) => (
     <header>
         <h1>Expensify</h1>
-        <NavLink to='/' activeClassName='is-active' exact={true}>Dashboard</NavLink>
+        <NavLink to='/dashboard' activeClassName='is-active' >Dashboard</NavLink>
         <NavLink to='/create' activeClassName='is-active'>Create Expense</NavLink>
         <button onClick={startLogout}>Log-out</button>
     </header>
--- src/components/HelpPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HelpPage = () => (
-    <div>
-        This is the help component
-    </div>
-);
-
-export default HelpPage;
